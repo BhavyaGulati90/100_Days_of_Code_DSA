4fc581bc0a2a275d8bd47f24ac8286e12e98de05
SohilSaini75 <sohilsaini075@gmail.com>
2026-02-27 13:44:40 +0530

    Day27_Ques

---
+++ Day27/Ques1.c
@@ -0,0 +1,105 @@
+/*Problem: Find Intersection Point of Two Linked Lists - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (first list)
+- Third line: integer m
+- Fourth line: m space-separated integers (second list)
+
+Output:
+- Print value of intersection node or 'No Intersection'*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+int getLength(struct Node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+int findIntersection(struct Node* head1, struct Node* head2) {
+    int len1 = getLength(head1);
+    int len2 = getLength(head2);
+
+    struct Node* temp1 = head1;
+    struct Node* temp2 = head2;
+
+    int diff;
+
+    if (len1 > len2) {
+        diff = len1 - len2;
+        while (diff--)
+            temp1 = temp1->next;
+    } else {
+        diff = len2 - len1;
+        while (diff--)
+            temp2 = temp2->next;
+    }
+
+    while (temp1 != NULL && temp2 != NULL) {
+        if (temp1->data == temp2->data)
+            return temp1->data;
+
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+
+    return -1; 
+}
+
+int main() {
+    int n, m, value;
+    struct Node* head1 = NULL;
+    struct Node* head2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head1 = insertEnd(head1, value);
+    }
+
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &value);
+        head2 = insertEnd(head2, value);
+    }
+
+    int result = findIntersection(head1, head2);
+
+    if (result == -1)
+        printf("No Intersection");
+    else
+        printf("%d", result);
+
+    return 0;
+}
