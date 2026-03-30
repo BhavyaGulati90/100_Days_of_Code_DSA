4eb5b81cede207afb3d4cd649e63ee27566a6766
SohilSaini75 <sohilsaini075@gmail.com>
2026-03-30 10:23:11 +0530

    Day58_Ques

---
+++ Day58/Ques1.c
@@ -0,0 +1,79 @@
+/*Problem Statement:
+Construct a binary tree from given preorder and inorder traversal arrays.
+
+Input Format:
+- First line contains integer N
+- Second line contains preorder traversal
+- Third line contains inorder traversal
+
+Output Format:
+- Print postorder traversal of constructed tree */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int findIndex(int inorder[], int start, int end, int value) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+struct Node* buildTree(int preorder[], int inorder[], int start, int end, int *preIndex) {
+    if (start > end)
+        return NULL;
+
+    struct Node* root = createNode(preorder[*preIndex]);
+    (*preIndex)++;
+
+    if (start == end)
+        return root;
+
+    int inIndex = findIndex(inorder, start, end, root->data);
+
+    root->left = buildTree(preorder, inorder, start, inIndex - 1, preIndex);
+    root->right = buildTree(preorder, inorder, inIndex + 1, end, preIndex);
+
+    return root;
+}
+
+void postorderTraversal(struct Node* root) {
+    if (root == NULL)
+        return;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int preorder[n], inorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &preorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    int preIndex = 0;
+    struct Node* root = buildTree(preorder, inorder, 0, n - 1, &preIndex);
+
+    postorderTraversal(root);
+    return 0;
+}
